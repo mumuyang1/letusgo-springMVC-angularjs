d545b297c55d39221aa4c65b35ffe92a373c2e4c
liyanzi <843624084@qq.com>
2014-11-14 11:46:46 +0800

    modify cartCounts show

--- src/main/webapp/client/app/scripts/controllers/index.js
+++ src/main/webapp/client/app/scripts/controllers/index.js
@@ -1,49 +1,50 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .controller('CartSumsCtrl', function ($scope,ItemsService, CartItemsService) {
+  .controller('CartSumsCtrl', function ($scope, ItemsService, CartItemsService) {
 
-      var temp = CartItemsService.get('cartSum');
-      var cartSum = temp ? parseInt(temp) : 0;
+    CartItemsService.getCartItemCounts(function (counts) {
+      var temp = CartItemsService.get('cartCounts');
+      var cartCounts = temp ? counts : temp;
+      CartItemsService.set('cartCounts', cartCounts);
+      $scope.cartsums = CartItemsService.get('cartCounts');
+    });
 
-      CartItemsService.set('cartSum',cartSum);
-      $scope.cartsums = CartItemsService.get('cartSum');
-
-      $scope.addCartSum = function(item){
-        $scope.cartsums = ItemsService.addCart(item);
-      };
-
-
-       $scope.$on('to-parent-add',function(add, item){
-
-          $scope.cartsums = CartItemsService.add(item);
-        });
+    $scope.addCartSum = function (item) {
+      $scope.cartsums = ItemsService.addCart(item);
+    };
 
 
-      $scope.$on('to-parent-reduce',function(reduce,cartItem){
+    $scope.$on('to-parent-add', function (add, item) {
 
-        CartItemsService.getCartItems(function(data) {
+      $scope.cartsums = CartItemsService.add(item);
+    });
 
-          $scope.cartsums = CartItemsService.updateCartSumsWhenReduce(data, cartItem)
-        });
+
+    $scope.$on('to-parent-reduce', function (reduce, cartItem) {
+
+      CartItemsService.getCartItems(function (data) {
+
+        $scope.cartsums = CartItemsService.updateCartSumsWhenReduce(data, cartItem)
       });
+    });
 
 
-    $scope.$on('to-parent-delete',function(event,id){
+    $scope.$on('to-parent-delete', function (event, id) {
 
-      CartItemsService.getCartItems(function(data) {
+      CartItemsService.getCartItems(function (data) {
         $scope.cartsums = CartItemsService.updateCartSumsWhenDelete(data, id);
       });
-     });
+    });
 
-    $scope.$on('to-parent-change',function(event,cartItem){
+    $scope.$on('to-parent-change', function (event, cartItem) {
 
-      CartItemsService.getCartItems(function(data) {
+      CartItemsService.getCartItems(function (data) {
         $scope.cartsums = CartItemsService.updateCartSumsWhenChange(data, cartItem);
       });
     });
 
-    function highlight(main,shoppingmall,cart,productManage) {
+    function highlight(main, shoppingmall, cart, productManage) {
       $scope.mainActive = main;
       $scope.shoppingMallActive = shoppingmall;
       $scope.cartActive = cart;
@@ -51,31 +52,31 @@ angular.module('letusgoApp')
     }
 
 
-    $scope.$on('to-parent-pay',function(){
+    $scope.$on('to-parent-pay', function () {
 
-      CartItemsService.pay(function(){
+      CartItemsService.pay(function () {
         $scope.cartsums = CartItemsService.get('cartSum');
       });
     });
 
 
     $scope.$on('to-parent-mainActive', function () {
-      highlight(true,false,false,false);
+      highlight(true, false, false, false);
     });
 
 
     $scope.$on('to-parent-shoppingMallActive', function () {
-      highlight(false,true,false,false);
+      highlight(false, true, false, false);
     });
 
 
     $scope.$on('to-parent-cartActive', function () {
-      highlight(false,false,true,false);
+      highlight(false, false, true, false);
     });
 
 
     $scope.$on('to-parent-productManageActive', function () {
-      highlight(false,false,false,true);
+      highlight(false, false, false, true);
     });
 
-   });
+  });
