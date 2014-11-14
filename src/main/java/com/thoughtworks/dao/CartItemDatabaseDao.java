57e8adeac6210b10c1a84bc820244a7d922dcec2
liyanzi <843624084@qq.com>
2014-11-14 19:44:01 +0800

    add getCartItem method and impl

--- src/main/java/com/thoughtworks/dao/CartItemDatabaseDao.java
+++ src/main/java/com/thoughtworks/dao/CartItemDatabaseDao.java
@@ -9,6 +9,8 @@ public interface CartItemDatabaseDao {
 
     public List<CartItemDatabase> getCartItems();
 
+    public CartItemDatabase getCartItem(int id);
+
     public void addCartItem(CartItem cartItem);
 
     public void modifyCartItem(CartItem cartItem);
--- src/main/java/com/thoughtworks/dao/impl/CartItemDatabaseDaoImpl.java
+++ src/main/java/com/thoughtworks/dao/impl/CartItemDatabaseDaoImpl.java
@@ -30,6 +30,19 @@ public class CartItemDatabaseDaoImpl implements CartItemDatabaseDao {
     }
 
     @Override
+    public CartItemDatabase getCartItem(int id) {
+        return jdbcTemplate.queryForObject("select * from cart_items where id = ?;", new Object[]{id}, new RowMapper<CartItemDatabase>() {
+            @Override
+            public CartItemDatabase mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new CartItemDatabase(
+                        rs.getInt("id"),
+                        rs.getInt("i_id"),
+                        rs.getDouble("count"));
+            }
+        });
+    }
+
+    @Override
     public void addCartItem(CartItem cartItem) {
         jdbcTemplate.update("insert into cart_items values (?,?,?);",cartItem.getId(),cartItem.getItem().getId(),cartItem.getCount());
     }
